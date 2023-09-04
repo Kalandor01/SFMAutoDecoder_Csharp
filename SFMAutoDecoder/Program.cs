bddb3fc55a939204870db466e6a22b0a081cf6a6
Kalandor01 <rohovszkyakoska@gmail.com>
2023-09-04 18:36:07 +0200

    added pure console based mode added suppont for files without an extension

--- SFMAutoDecoder/Program.cs
+++ SFMAutoDecoder/Program.cs
@@ -8,12 +8,21 @@ internal class Program
         {
             Utils.PressKey("Open a \".savc\"(or similarly encoded) file with this exe.");
         }
-        else
+        // manual mode
+        else if (args.Length == 1)
         {
             var filePath = args[0];
-            var fileExt = Path.GetExtension(filePath)[1..];
-            var filePathStripped = filePath[..(filePath.Length - fileExt.Length - 1)];
-            var fileName = Path.GetFileNameWithoutExtension(filePath);
+            var fileExt = Path.GetExtension(filePath);
+            string filePathStripped;
+            if (fileExt != "")
+            {
+                fileExt = fileExt[1..];
+                filePathStripped = filePath[..(filePath.Length - fileExt.Length - 1)];
+            }
+            else
+            {
+                filePathStripped = filePath;
+            }
             var fileSeed = Utils.ReadInt("What is the seed?: ");
 
             List<string> fileData;
@@ -36,7 +45,8 @@ internal class Program
             {
                 Console.WriteLine(line);
             }
-            if (Utils.Input($"\n\nDo you want to save the results as \"{fileName}.decoded.json\"?(Y/N): ").ToUpper() == "Y")
+            var fileName = Path.GetFileNameWithoutExtension(filePath);
+            if (Utils.Input($"\n\nDo you want to save the results as \"{fileName}.decoded.json\"?(Y/N): ")?.ToUpper() == "Y")
             {
                 using (var f = new StreamWriter($"{filePathStripped}.decoded.json"))
                 {
@@ -46,6 +56,54 @@ internal class Program
                     }
                 }
                 Utils.PressKey("SAVED!");
+            }
+        }
+        // auto mode
+        else
+        {
+            if (!File.Exists(args[0]))
+            {
+                return;
+            }
+
+            var filePath = args[0];
+            var fileExt = Path.GetExtension(filePath);
+            string filePathStripped;
+            if (fileExt != "")
+            {
+                fileExt = fileExt[1..];
+                filePathStripped = filePath[..(filePath.Length - fileExt.Length - 1)];
+            }
+            else
+            {
+                filePathStripped = filePath;
+            }
+
+            if (!long.TryParse(args[1], out long fileSeed))
+            {
+                throw new ArgumentException("File seed argument is not a long value");
+            }
+
+            var fileData = FileConversion.DecodeFile(fileSeed, filePathStripped, fileExt);
+
+            if (
+                args.Length >= 3 &&
+                bool.TryParse(args[2], out bool saveToFile) &&
+                saveToFile
+            )
+            {
+                using var f = new StreamWriter($"{filePathStripped}.decoded.json");
+                foreach (var line in fileData)
+                {
+                    f.WriteLine(line);
+                }
+            }
+            else
+            {
+                foreach (var line in fileData)
+                {
+                    Console.WriteLine(line);
+                }
             }
         }
     }
